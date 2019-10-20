69775f4394bb9c49b1902eec0cafd503b13acb02
Geovic Fagel <geofag@jpj.com>
2019-10-20 21:57:41 +0800

    useEffect Render Once

---
+++ src/components/HookMouse.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from 'react';
+
+const HookMouse = () => {
+    const [x, setx]=useState(0)
+    const[y,sety] =useState(0)
+
+
+    const logMousePosition = e => {
+        console.log('Mouse Event')
+        setx(e.clientX)
+        sety(e.clientY)
+        }
+    
+    useEffect(()=> {
+        window.addEventListener('mousemove', logMousePosition)
+    },[] ) //Only rerender once
+    return (
+        <div>
+             X- {x} Y-{y}
+        </div>
+    );
+}
+
+export default HookMouse;
